1ee2e7732d3be58a86bd1e46f42e79cb6497c8c4
Lucas Montecino <lucasmontecino95@gmail.com>
2025-05-06 11:44:40 -0300

    Refactor notes API to use MongoDB with Mongoose, replacing in-memory storage and adding error handling

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
-const cors = require('cors');
+const mongoose = require('mongoose');
+const Note = require('./models/note');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -26,37 +28,38 @@ let notes = [
 app.use(express.json());
 app.use(express.static('dist'));
 app.use(morgan('tiny'));
-app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>');
 });
 
 app.get('/api/notes', (req, res) => {
-  res.json(notes);
+  Note.find({}).then((notes) => {
+    res.json(notes);
+  });
 });
 
 app.get('/api/notes/:id', (req, res) => {
   const { id } = req.params;
-  const findNote = notes.find((note) => note.id === id);
-
-  if (findNote) {
-    res.json(findNote);
-  } else {
-    res.status(404).send(`No resource find with id: ${id}`);
-  }
+  Note.findOne({ _id: id }).then((note) => {
+    if (!note)
+      return res
+        .status(404)
+        .send(`No resource find with id: ${id}`);
+    else res.json(note);
+  });
 });
 
 app.delete('/api/notes/:id', (req, res) => {
   const { id } = req.params;
 
-  const findNote = notes.find((note) => note.id === id);
-  if (findNote) {
-    notes = notes.filter((note) => note.id !== id);
-    res.status(204).end();
-  } else {
-    res.status(404).send(`No resource find with id: ${id}`);
-  }
+  Note.deleteOne({ _id: id }).then((note) => {
+    if (!note)
+      return res
+        .status(404)
+        .send(`No resource find with id: ${id}`);
+    else res.status(204).end();
+  });
 });
 
 app.post('/api/notes', (req, res) => {
@@ -68,15 +71,14 @@ app.post('/api/notes', (req, res) => {
     });
   }
 
-  const newNote = {
-    id: crypto.randomUUID(),
+  const note = new Note({
     content,
     important: important || false,
-  };
+  });
 
-  notes = notes.concat(newNote);
-
-  res.status(201).json(newNote);
+  note.save().then((result) => {
+    res.status(201).json(result);
+  });
 });
 
 app.listen(PORT, () => {
