f5a4f541dc2f10d4898b8784e98904b918b96f7d
Lucas Montecino <lucasmontecino95@gmail.com>
2025-05-07 15:51:57 -0300

    Refactor note schema to enforce content validation with minLength and required fields

--- models/note.js
+++ models/note.js
@@ -4,7 +4,6 @@ mongoose.set('strictQuery', false);
 
 const url = process.env.MONGODB_URI;
 
-console.log('connecting to', url);
 mongoose
   .connect(url)
 
@@ -19,7 +18,11 @@ mongoose
   });
 
 const noteSchema = new mongoose.Schema({
-  content: String,
+  content: {
+    type: String,
+    minLength: 5,
+    required: true,
+  },
   important: Boolean,
 });
 
