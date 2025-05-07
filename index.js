27755d32635ebd1332b075bb7ff280a7dc70c3e9
Lucas Montecino <lucasmontecino95@gmail.com>
2025-05-07 15:52:03 -0300

    Refactor API routes for better error handling and response consistency

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
-const mongoose = require('mongoose');
 const Note = require('./models/note');
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -29,6 +28,20 @@ app.use(express.json());
 app.use(express.static('dist'));
 app.use(morgan('tiny'));
 
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message);
+
+  if (error.name === 'CastError') {
+    return res
+      .status(400)
+      .send({ error: 'malformatted id' });
+  } else if (error.name === 'ValidationError') {
+    return res.status(400).json({ error: error.message });
+  }
+
+  next(error);
+};
+
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>');
 });
@@ -39,48 +52,69 @@ app.get('/api/notes', (req, res) => {
   });
 });
 
-app.get('/api/notes/:id', (req, res) => {
+app.get('/api/notes/:id', (req, res, next) => {
   const { id } = req.params;
-  Note.findOne({ _id: id }).then((note) => {
-    if (!note)
-      return res
-        .status(404)
-        .send(`No resource find with id: ${id}`);
-    else res.json(note);
-  });
+  Note.findById(id)
+    .then((note) => {
+      if (!note) return res.status(404).end();
+      else res.json(note);
+    })
+    .catch((err) => next(err));
 });
 
-app.delete('/api/notes/:id', (req, res) => {
+app.delete('/api/notes/:id', (req, res, next) => {
   const { id } = req.params;
 
-  Note.deleteOne({ _id: id }).then((note) => {
-    if (!note)
-      return res
-        .status(404)
-        .send(`No resource find with id: ${id}`);
-    else res.status(204).end();
-  });
+  Note.findByIdAndDelete(id)
+    .then((note) => {
+      if (!note)
+        return res
+          .status(404)
+          .send(`No resource find with id: ${id}`);
+      else res.status(204).end();
+    })
+    .catch((err) => next(err));
 });
 
-app.post('/api/notes', (req, res) => {
+app.post('/api/notes', (req, res, next) => {
   const { content, important } = req.body;
-
-  if (!content) {
-    return res.status(400).json({
-      message: 'Please give a valid content for the note.',
-    });
-  }
 
   const note = new Note({
     content,
     important: important || false,
   });
 
-  note.save().then((result) => {
-    res.status(201).json(result);
-  });
+  note
+    .save()
+    .then((result) => {
+      res.status(201).json(result);
+    })
+    .catch((error) => next(error));
 });
 
+app.put('/api/notes/:id', (req, res, next) => {
+  const { content, important } = req.body;
+  const { id } = req.params;
+  Note.findById(id)
+    .then((note) => {
+      if (!note) {
+        return res.status(404).end();
+      }
+
+      note.content = content;
+      note.important = important;
+
+      return note.save().then((updatedNote) => {
+        res.json(updatedNote);
+      });
+    })
+    .catch((err) => next(err));
+});
+
+app.use(errorHandler);
+
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(
+    `Server running on port http://localhost:${PORT}`
+  );
 });
