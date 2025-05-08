beb308c3da7d830335eb7e34faff3430071bb1db
Lucas Montecino <lucasmontecino95@gmail.com>
2025-05-08 09:28:04 -0300

    Add middleware for request logging and error handling

---
+++ utils/middleware.js
@@ -0,0 +1,28 @@
+const morgan = require('morgan');
+const logger = require('../utils/logger');
+
+const requestLogger = morgan('dev');
+
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: 'unknown endpoint' });
+};
+
+const errorHandler = (error, req, res, next) => {
+  logger.error(error.message);
+
+  if (error.name === 'CastError') {
+    return res
+      .status(400)
+      .send({ error: 'malformatted id' });
+  } else if (error.name === 'ValidationError') {
+    return res.status(400).json({ error: error.message });
+  }
+
+  next(error);
+};
+
+module.exports = {
+  requestLogger,
+  errorHandler,
+  unknownEndpoint,
+};
